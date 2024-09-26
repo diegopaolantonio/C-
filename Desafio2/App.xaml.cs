ee86faab831301facd7598764df0ce79a75b1903
diegopaolantonio <dpaolantonio.expertoing@gmail.com>
2024-09-25 23:14:26 -0300

    Commit 1

---
+++ Desafio2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Desafio2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Desafio2/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Desafio2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
